dbd6652e55c1464360a8221d424482afad08f165
Strannik73 <Santon@tutamail.com>
2024-12-06 11:02:16 +0300

    circle.py

---
+++ sum.py
@@ -0,0 +1,10 @@
+def ls (a):
+    print(sum(a))# выводим сумму элиментов списка (a)
+    return(a) 
+
+# создаем рандомнные числа и помещаем их в список
+from random import randint
+a = [randint(1, 20) for i in range(10)] 
+
+print(a)# выводим список, чтобы видеть какие числа в нем существуют
+fnc = ls (a)
