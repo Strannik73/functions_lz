bb1e4abb43f60fc826461c94b2de008a8e9565c1
Strannik73 <Santon@tutamail.com>
2024-12-06 12:06:26 +0300

    min_max_mid.py

--- min_max_mid.py
+++ min_max_mid.py
@@ -3,21 +3,20 @@ def ms (a):
 
         print('минимальное:  ' ,min(a), )# минимальное значение
         print('максимальное:  ' ,max(a), )# максимальное значение
-        print('среднее:  ' ,float(sum(a)/2 ), )# среднее значение
+        print('среднее:  ' ,float(sum(a)/len(a) ), )# среднее значение
         return(a) 
 
 
 
 
-
+n = input('введите кол-во элиментов:  ')
 from random import randint
-p = [randint(1, 10) for i in range(20)] 
+p = [randint(1, n+1) for i in range(20)] 
 print(p)
 
 
-srd=float(sum(p)/2 )
-for i in range(0, (i+1)):  
-        i = n
+#for i in range(1, data[])
+
 
 
 fnc = ms (p)
