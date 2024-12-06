291793fad8c043c6ddaf4d134cec622189e9f829
Strannik73 <Santon@tutamail.com>
2024-12-06 11:52:01 +0300

    times.py

--- min_max_mid.py
+++ min_max_mid.py
@@ -1,10 +1,15 @@
 
 def ms (a):
-    print('минимальное:  ' ,min(a), )# минимальное значение
-    print('максимальное:  ' ,max(a), )# максимальное значение
-    print('среднее:  ' ,float(sum(a)/2 ), )# среднее значение
-    return(a) 
-   
+
+        print('минимальное:  ' ,min(a), )# минимальное значение
+        print('максимальное:  ' ,max(a), )# максимальное значение
+        print('среднее:  ' ,float(sum(a)/2 ), )# среднее значение
+        return(a) 
+
+for i in range(0, ):  
+        i =
+
+
 from random import randint
 p = [randint(1, 10) for i in range(20)] 
 print(p)
